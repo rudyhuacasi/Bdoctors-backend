1613080deb0812df689f02d19930d42313100353
rudyhuacasi <alexisitaliano16@gmail.com>
2024-10-11 10:52:44 +0200

    {UPD} added info of the seeder

---
+++ config/specializations.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'specializations' => [
+        'Cardiology',
+        'Neurology',
+        'Pediatrics',
+        'Orthopedics',
+        'Dermatology',
+        'Radiology',
+        'Gastroenterology',
+        'Oncology',
+        'Psychiatry',
+        'Endocrinology',
+        'Urology',
+        'Obstetrics and Gynecology',
+        'Ophthalmology',
+        'ENT (Ear, Nose, and Throat)',
+        'Anesthesiology',
+        'Family Medicine',
+        'Internal Medicine',
+        'General Surgery',
+        'Emergency Medicine',
+        'Pathology',
+    ],
+];
---
+++ config/sponsorships.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    [
+        'package' => '24 ore',
+        'price' => 2.99,
+    ],
+    [
+        'package' => '72 ore',
+        'price' => 5.99,
+    ],
+    [
+        'package' => '144 ore',
+        'price' => 9.99,
+    ],
+];
